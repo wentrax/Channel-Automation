408a1b5db432ffe7d72b7cbcdc776ec4cef0b956
A - L - E - X <93909675+Lx0988@users.noreply.github.com>
2022-12-18 04:01:08 +0530

    Create  userforward.py

---
+++ AutoForward/plugins/userforward.py
@@ -0,0 +1,20 @@
+import logging
+logger = logging.getLogger(__name__)
+from AutoForward.config import Config
+import asyncio
+from pyrogram import Client, filters
+
+
+@Client.on_message(filters.channel)
+async def forward(client, message):
+    # Forwarding the messages to the channel
+   try:
+      for id in Config.CHANNEL:
+         from_channel, to_channel = id.split(":")
+         if message.chat.id == int(from_channel):
+#           func = message.copy if False else message.forward
+            await message.copy(int(to_channel))
+            print("Forwarded a message from", from_channel, "to", to_channel)
+            await asyncio.sleep(1)
+   except Exception as e:
+      logger.exception(e)
