c087119474618dbc5585b2106167eb55aba45152
A - L - E - X <93909675+Lx0988@users.noreply.github.com>
2022-12-18 04:03:36 +0530

    Update __init__.py

--- AutoForward/__init__.py
+++ AutoForward/__init__.py
@@ -8,6 +8,8 @@ APP_ID = int(os.environ.get("APP_ID"))
 API_HASH = os.environ.get("API_HASH")
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 USER_SESSION = os.environ.get("USER_SESSION")
+MAINCHANNEL_ID = None
+
 
 VERIFY = {}
 
