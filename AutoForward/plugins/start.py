94fedb8f557ee2522bc03b0b7f22a1c60d5dc385
A - L - E - X <93909675+Lx0988@users.noreply.github.com>
2022-12-18 11:34:15 +0530

    Update start.py

--- AutoForward/plugins/start.py
+++ AutoForward/plugins/start.py
@@ -1,5 +1,5 @@
 from pyrogram import Client, filters
 
-@autocaption.on_message(filters.command("start") & filters.incoming)
+@Client.on_message(filters.command("start") & filters.incoming)
 async def start(bot, message):
         await message.reply(" Hello, I'm Auto Forward Bot")
