4668605b75d1efc1f80653b96fe769faf42dd72a
A - L - E - X <93909675+Lx0988@users.noreply.github.com>
2022-12-18 13:47:45 +0530

    Update bot.py

--- AutoForward/bot.py
+++ AutoForward/bot.py
@@ -3,29 +3,25 @@ from pyrogram import Client, enums, __version__
 from AutoForward import API_HASH, APP_ID, LOGGER, BOT_TOKEN 
 
 class Bot(Client):
-
     def __init__(self):
         super().__init__(
-            "bot",
+            name='bot',
+            bot_token=BOT_TOKEN,
             api_hash=API_HASH,
             api_id=APP_ID,
-            plugins={
-                "root": "AutoForward/plugins"
-            },
-            workers=200,
-            bot_token=BOT_TOKEN,
-            sleep_threshold=10
+            workers=8
         )
         self.LOGGER = LOGGER
 
     async def start(self):
         await super().start()
-        bot_details = await self.get_me()
+        myusername = await self.get_me()
         self.set_parse_mode(enums.ParseMode.HTML)
         self.LOGGER(__name__).info(
-            f"@{bot_details.username}  started! "
+            f"@{myusername.username}  started!"
         )
         
+
     async def stop(self, *args):
         await super().stop()
         self.LOGGER(__name__).info("Bot stopped. Bye.")
