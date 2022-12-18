dc7992fcb29aea31248fb65065ba2048753e3bb8
A - L - E - X <93909675+Lx0988@users.noreply.github.com>
2022-12-18 14:25:13 +0530

    Update client.py

--- AutoForward/client.py
+++ AutoForward/client.py
@@ -1,34 +1,34 @@
 from pyrogram import Client, enums, __version__
 from AutoForward import API_HASH, APP_ID, LOGGER, \
     USER_SESSION
-from AutoForward.bot import Lxbot
+# from AutoForward.bot import Lxbot
 
-class User(Client):
-    LXBOT: Lxbot = "FlashV2Robot"
-    LXBOT_ID = 5300015911
+class Bot(Client):
+#    USER: User = None
+#    USER_ID: int = None
 
     def __init__(self):
-        super().__init__(            
-            name='user_session',
-            session_string=USER_SESSION,
+        super().__init__(
+            "User1",
             api_hash=API_HASH,
             api_id=APP_ID,
+            session_string=USER_SESSION,
             workers=200,            
             sleep_threshold=10,
             plugins={
-                "root": "AutoForward/plugins"
+                "root": "bot/plugins"
             }
         )
         self.LOGGER = LOGGER
 
     async def start(self):
         await super().start()
-        user_details = await self.get_me()
+        bot_details = await self.get_me()
         self.set_parse_mode(enums.ParseMode.HTML)
         self.LOGGER(__name__).info(
-            f"@{user_details.username}  started! "
+            f"@{bot_details.username}  started! "
         )
-        LXBOT, LXBOT_ID = await Lxbot().start()
+#        self.USER, self.USER_ID = await User().start()
 
     async def stop(self, *args):
         await super().stop()
