2e2e7bd15741ad8bc929f0b80d06e7cf884075a2
A - L - E - X <93909675+Lx0988@users.noreply.github.com>
2022-12-18 15:30:05 +0530

    Update UserClients.py

--- AutoForward/UserClients.py
+++ AutoForward/UserClients.py
@@ -1,24 +1,35 @@
 from pyrogram import Client, enums, __version__
 
-from AutoForward import API_HASH, APP_ID, LOGGER, USER_2_SESSION
+from AutoForward import API_HASH, APP_ID, LOGGER, BOT_TOKEN 
 
-class User2nd(Client):
+from AutoForward.client import User
+
+class Bot(Client):
+    USER: User = None
+    USER_ID: int = None
+
     def __init__(self):
         super().__init__(
-            "userbot",
+            "bot",
             api_hash=API_HASH,
             api_id=APP_ID,
-            session_string=USER_2_SESSION,
-            workers=20
+            plugins={
+                "root": "bot/plugins"
+            },
+            workers=200,
+            bot_token=BOT_TOKEN,
+            sleep_threshold=10
         )
         self.LOGGER = LOGGER
 
     async def start(self):
         await super().start()
-        try: await self.export_session_string()
-        except: pass
-        usr_bot_me = await self.get_me()
-        return (self, usr_bot_me.id)
+        bot_details = await self.get_me()
+        self.set_parse_mode(enums.ParseMode.HTML)
+        self.LOGGER(__name__).info(
+            f"@{bot_details.username}  started! "
+        )
+        self.USER, self.USER_ID = await User().start()
 
     async def stop(self, *args):
         await super().stop()
