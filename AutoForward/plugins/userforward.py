2b0aeb8a72ecfdeab41d9e88db1eb10499b8193e
A - L - E - X <93909675+Lx0988@users.noreply.github.com>
2022-12-18 11:39:15 +0530

    Update userforward.py

--- AutoForward/plugins/userforward.py
+++ AutoForward/plugins/userforward.py
@@ -9,7 +9,7 @@ CHANNEL_ID = 1001743048821
 @Client.on_message(filters.media)
 async def forward(bot, update):
     try:      
-        await bot.copy_message(
+        await bot.USER.copy_message(
             chat_id=-1001743048821,
             from_chat_id=update.chat.id,
             message_id=update.id,
