b68cce75c7b99e6b1f6ddb593ec5ec1bea4c81b6
A - L - E - X <93909675+Lx0988@users.noreply.github.com>
2022-12-18 13:20:52 +0530

    Create autoedite.py

---
+++ AutoForward/plugins/autoedite.py
@@ -0,0 +1,34 @@
+# @Lx_0988
+import logging
+from pyrogram import Client, filters, enums
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+media_filter = filters.document | filters.video | filters.audio
+
+@Client.on_message(filters.chat(-1001743048821) & media_filter)
+async def editing(bot, message):
+      try:
+         media = message.document or message.video or message.audio
+         caption_text = "@HQFilms4u"
+      except:
+         caption_text = ""
+         pass 
+      if (message.document or message.video or message.audio): 
+          if message.caption:                        
+             file_caption = f"**{message.caption}**"                
+          else:
+             fname = media.file_name
+             filename = fname.replace("_", ".")
+             file_caption = f"`{filename}`"  
+              
+      try:          
+             await bot.edit_message_caption(
+                 chat_id=message.chat.id, 
+                 message_id=message.id,
+                 caption=file_caption + "\n" + "@HQFilms4U",
+                 parse_mode=enums.ParseMode.MARKDOWN
+             )
+
+      except:
+          pass
