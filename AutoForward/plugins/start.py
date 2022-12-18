c68811f364426643e8822e77d901c76bc8c206a1
A - L - E - X <93909675+Lx0988@users.noreply.github.com>
2022-12-18 11:12:23 +0530

    Create start.py

---
+++ AutoForward/plugins/start.py
@@ -0,0 +1,5 @@
+from program import Clinet, filters
+
+@Clinet.on_message(filters.command("start") & filters.private)
+async def start(bot, cmd):
+      await bot.send_message("Hi there, I'm Auto forward bot")
