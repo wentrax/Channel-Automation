850d178a5e4dd63f001d2ffea464fd1d51820209
A - L - E - X <93909675+Lx0988@users.noreply.github.com>
2022-12-18 15:23:13 +0530

    Update __client__.py

--- AutoForward/__client__.py
+++ AutoForward/__client__.py
@@ -1,12 +1,8 @@
 from pyrogram import Client, enums, __version__
 from AutoForward import API_HASH, APP_ID, LOGGER, \
     USER_SESSION
-from AutoForward.UserClients import User2nd
 
 class User(Client):
-    USER2ND: User2nd = None
-    USER2ND_ID: int = None
-
     def __init__(self):
         super().__init__(
             "userbot",
@@ -21,17 +17,12 @@ class User(Client):
         self.LOGGER = LOGGER
 
     async def start(self):
-        await self.session.start()
+        await super().start()
         try: await self.export_session_string()
         except: pass
         usr_bot_me = await self.get_me()
         return (self, usr_bot_me.id)
-        self.USER2ND, self.USER2ND_ID = await self.connect()
 
     async def stop(self, *args):
         await super().stop()
         self.LOGGER(__name__).info("Bot stopped. Bye.")
-
-
-app = User()
-app.run(self.start())
