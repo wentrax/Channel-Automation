0029ec1a1eb38a4e70e583fea610490a60ef3468
A - L - E - X <93909675+Lx0988@users.noreply.github.com>
2022-12-18 16:32:34 +0530

    Update userforward.py

--- AutoForward/plugins/userforward.py
+++ AutoForward/plugins/userforward.py
@@ -1,18 +1,17 @@
 import logging, asyncio
 logger = logging.getLogger(__name__)
-from AutoForward import MAINCHANNEL_ID
+from AutoForward import FROMCHANNEL_ID
 from pyrogram import Client, filters, enums
 from pyrogram.errors import FloodWait
 
-CHANNEL_ID = 1001743048821
-
+ 
 @Client.on_message(filters.media)
 async def forward(client, update):
     try:      
         await asyncio.sleep(10)
         await client.copy_message(
-            chat_id=-1001743048821,
-            from_chat_id=update.chat.id,
+            chat_id=-1001688669689,
+            from_chat_id=FROMCHANNEL_ID,
             message_id=update.id,
             caption=update.caption,
             parse_mode=enums.ParseMode.MARKDOWN
