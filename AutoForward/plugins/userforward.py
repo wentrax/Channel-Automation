ab828964b4cf32a3173453f3ffc2dd20de2481ce
A - L - E - X <93909675+Lx0988@users.noreply.github.com>
2022-12-18 12:40:08 +0530

    Update userforward.py

--- AutoForward/plugins/userforward.py
+++ AutoForward/plugins/userforward.py
@@ -3,13 +3,14 @@ logger = logging.getLogger(__name__)
 from AutoForward import MAINCHANNEL_ID
 from pyrogram import Client, filters, enums
 from AutoForward.client import User
+from pyrogram.errors import FloodWait
 
 CHANNEL_ID = 1001743048821
 
-@User.on_message(filters.media)
+@Client.on_message(filters.media)
 async def forward(client, update):
     try:      
-        await User.copy_message(
+        await client.copy_message(
             chat_id=-1001743048821,
             from_chat_id=-1001531149575,
             message_id=update.id,
