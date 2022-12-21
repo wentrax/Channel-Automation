d11f2059f6523c538d7dcf0fd3b7296f3b8cee26
A - L - E - X <93909675+Lx0988@users.noreply.github.com>
2022-12-22 00:51:39 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -8,20 +8,23 @@ I am very useful for the channel admin who have many channels.😁
 
 <b>Maintained By:</b> <a href="https://t.me/Lx_0980">Lx 0980</a>
 """    
+      COMMANDS_TEXT = """
+      <b><u>AVAILABLE COMMAND</b></u>
 
-      HELP_TEXT = """
-<u>💡 𝐇𝐞𝐥𝐩</u> 
-👉 Add me as a Admin in your channel with edit permission
-👉 Add your caption using <code>/set_caption</code> command
-<i>( Only Admin )</i>
-   
-<i>(Support Markdown)</i>
-👉 Forward your files in your channel and I will edit it
-<b><a href="https://t.me/Ts_bots">©️ ᴛʀᴀᴄᴋsᴛᴜᴅɪᴏ's ʙᴏᴛs</a></b>
-"""    
-              
+• /start - <b>Bot Alive</b>
 
-     
+• /help - <b>more help</b>
+
+• /run - <b>start forward</b>
+
+• /about - <b>About Me</b>
+
+• /info - <b>get your telegram ID info.
+
+• /id - <b>Your telegram ID</b
+"""
+
+
       ABOUT_TEXT = """
 MY DETAILS:
 
@@ -36,46 +39,3 @@ MY DETAILS:
 👥 Group:<b> <b><a href="https://t.me/Hollywood_0980">Hollywood 0980</a></b>
 """
 
-# Bot status display
-
-      AUTOCAPTION_STATUS = """
-⭕️ <u>𝐒𝐓𝐀𝐓𝐔𝐒</u>
-• <b>Current Caption :</b>
-{}
-• <b>Current Position :</b> {}
-
-      FILES_FORWARDER_STATUS = """
-Auto file forward bot. Without Admin Permission in FROM_CHANNEL Only Give Permission In your Telegram Personal Channel
-
-⭕️ <u>𝐒𝐓𝐀𝐓𝐔𝐒</u>
-<b>• FROM CHANNEL :</b> {}
-<b>• TO CHANNEL :</b> {}
-"""
-      AUTO_FORWARD_STATUS = """
-I'm forward your files from one channel to other channel when a new post is available.
-
-⭕️ <u>𝐒𝐓𝐀𝐓𝐔𝐒</u>
-<b>• FROM CHANNELS :</b> {}
-<b>• TO CHANNELS :</b> {}
-"""
-      MEDIA_COPY_STATUS = """
-I can copy any media to a destination chat provided!
-
-<b>𝖶𝖮𝖱𝖪𝖨𝖭𝖦:</b>
-• Add The Bot to Your destination channel as admin
-• You can add Bot as admin to any chat (Groups or Channels)
-• Bot will copy any media content send in to the above chats to the destination Channel !
-•For the above, You need to give a valid channel Id starting with '-100' in the var "Channel_ID"
-
-⭕️ <u>𝐒𝐓𝐀𝐓𝐔𝐒</u>
-<b>• CURRENT ID</b> : {}
-"""
-
-      AUTO_FILTER = """
-I can search for the files from provided channel according to given query and gives link to those files as buttons!
-
-      HELP_TXT = """<b>Follow These Steps!!</b>
-<b>• First deploy your files forward bot.
-<b>• Then give admin permission in your personal telegram channel</b>
-<b>• Then send any message In your personal telegram channel</b>
-<b>• Then use /run command in your bot</b>
