bcdbb1648f6aec7f7e9774d102a55f77115cd4b5
A - L - E - X <93909675+Lx0988@users.noreply.github.com>
2022-12-22 02:00:46 +0530

    Update info.py

--- bot/info.py
+++ bot/info.py
@@ -1,13 +1,12 @@
-CAPTION_POSITION = "bottom"
+class Config(object):
+      CAPTION_POSITION = "bottom"
 
-ADMIN_USERNAME = "TTYUIOPTSREW"
-
-ADMIN_ID = 1903280447
-
-DB_URL = "mongodb+srv://Channel-Automation-Robot:seCWS0GPZS4WIveQ@cluster0.r8flwuy.mongodb.net/?retryWrites=true&w=majority"
+      ADMIN_USERNAME = "TTYUIOPTSREW"
+      ADMIN_ID = 1903280447
+      DB_URL = "mongodb+srv://Channel-Automation-Robot:seCWS0GPZS4WIveQ@cluster0.r8flwuy.mongodb.net/?retryWrites=true&w=majority"
      
-FROM_CHANNEL = "@bdbdhdhvh" 
-      
-TO_CHANNEL = -1001531149575
+      FROM_CHANNEL = "@bdbdhdhvh"       
+      TO_CHANNEL = -1001531149575
+      FILES_CAPTION = "HQFilms4U'
 
-FILES_CAPTION = "HQFilms4U"
+      FILTERCHANNEL_ID =  -1001743048821   
