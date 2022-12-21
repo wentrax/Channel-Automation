ca61dccbc59c4b1311f8e534df00bfb5a273a4ef
A - L - E - X <93909675+Lx0988@users.noreply.github.com>
2022-12-22 00:29:52 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -3,10 +3,10 @@ class Translation(object):
       
       START_TEXT = """
 😃 Hai {},
-I am a simple auto caption bot
-I can automatically add pre-setted caption to the files in channels
-I am currently working for a channel if you want to use me, Use my source code and Deploy me
-⚜️ Maintained By 👉 @{}
+I am a simple auto Channel Automation bot
+I am very useful for the channel admin who have many channels.😁
+
+<b>Maintained By:</b> <a href="https://t.me/Lx_0980">Lx 0980</a>
 """    
 
       HELP_TEXT = """
@@ -23,47 +23,59 @@ I am currently working for a channel if you want to use me, Use my source code a
 
      
       ABOUT_TEXT = """
-📕 --𝐀𝐛𝐨𝐮𝐭-- --𝐌𝐞--
-○ ᴍʏ ɴᴀᴍᴇ : [Auto Caption | Ts Bots](https://t.me/Anylink_Movies)
-○ ʟᴀɴɢᴜᴀɢᴇ : ᴘʏᴛʜᴏɴ 
-○ ғʀᴀᴍᴇᴡᴏʀᴋ : ᴘʏʀᴏɢʀᴀᴍ 
-○ sᴇʀᴠᴇʀ : ʜᴇʀᴏᴋᴜ 
-○ ᴠᴇʀsɪᴏɴ : 1.1.0
-○ sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ  : [ᴛᴏᴜᴄʜ ᴍᴇ 🤗](https://github.com/Ts-bots/AutoCaption-Ts_Bot-Clone)
-○ ᴄʀᴇᴀᴛᴏʀ :  [ᴛʀᴀᴄᴋsᴛᴜᴅɪᴏ](https://t.me/trackstudio)
- 
-__ᴘʟᴇᴀsᴇ ɴᴏᴛᴇ ɪ ᴀᴍ ɴᴏᴛ ᴀ ᴘʀᴏ ᴄᴏᴅᴇʀ ᴀɴᴅ ᴛʜɪs ɪs ᴍʏ ғɪʀsᴛ ᴛɪᴍᴇ ɪ ᴀᴍ ɢɪᴠɪɴɢ ᴀɴ ᴏᴘᴇɴ sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ ᴏғ ʙᴏᴛ ғᴏʀ ᴇᴠᴇʀʏᴏɴᴇ ᴡʜɪᴄʜ ᴡᴀs ᴡʀɪᴛᴇʀ ɪɴ ᴍʏ ᴏᴡɴ ʜᴀɴᴅs, sᴏ ɪғ ᴀɴʏ ʙᴜɢs ғᴇᴇʟ ғʀᴇᴇ ᴛᴏ sᴀʏ
-ɪᴛ ᴍᴀʏ ʙᴇ ɴᴏᴛ ᴛʜᴇ ʙᴇsᴛ ʙᴜᴛ ғᴏʀ ᴍᴇ ɪᴛ ɪs ᴛʜᴇ ʙᴇsᴛ 🥰
-ᴛʜᴀɴᴋs ᴛᴏ ᴍʏ ʙᴇʟᴏᴠᴇᴅ ғʀɪᴇɴᴅ [ᴀɴᴏɴʏᴍᴏᴜs](https://t.me/Anoymous_Ns)__
-**[©️ ᴛʀᴀᴄᴋsᴛᴜᴅɪᴏ's ʙᴏᴛs](https://t.me/ts_bots)**
-"""
+MY DETAILS:
 
-      MARKDOWN_TEXT = """
-🔰 <u>𝐀𝐛𝐨𝐮𝐭 𝐌𝐚𝐫𝐤𝐝𝐨𝐰𝐧</u>
-👉 <b>Bold text</b>
-      
-📌 <code>**text**</code> 
-👉 <b>Italic text</b>
-📌 <code>__text__</code> 
-👉 <b>Code text</b>
-📌 <code>`text`</code>   
-👉 <b>Hyperlink text</b>
-📌 <code>[hyperlink_text](https://t.me/Ts_bots)</code> 
-<b><a href="https://t.me/Ts_bots">©️ ᴛʀᴀᴄᴋsᴛᴜᴅɪᴏ's ʙᴏᴛs</a></b>
+</b>🤖 My Name:</b> Channel Automation 
+
+<b>📝 Language:<b> <b><a href="https://www.python.org/">Python</a></b>
+
+<b>🧰 Framework:<b> <b><a href="https://github.com/pyrogram/pyrogram">Pyrogram 2.0.69</a></b>
+
+<b>📢 Channel:<b> <b><a href="https://t.me/Lx0980_Official">Lx0980</a></b>
+
+👥 Group:<b> <b><a href="https://t.me/Hollywood_0980">Hollywood 0980</a></b>
 """
 
 # Bot status display
 
-      STATUS_DATA = """
-🔰 <u>𝐁𝐎𝐓 𝐒𝐓𝐀𝐓𝐔𝐒</u>
-🖌️ <b>Current Caption :</b>
+      AUTOCAPTION_STATUS = """
+⭕️ <u>𝐒𝐓𝐀𝐓𝐔𝐒</u>
+• <b>Current Caption :</b>
 {}
-📐 <b>Current Position :</b> {}
-<b><a href="https://t.me/Ts_bots">©️ ᴛʀᴀᴄᴋsᴛᴜᴅɪᴏ's ʙᴏᴛs</a></b>
+• <b>Current Position :</b> {}
+
+      FILES_FORWARDER_STATUS = """
+Auto file forward bot. Without Admin Permission in FROM_CHANNEL Only Give Permission In your Telegram Personal Channel
+
+⭕️ <u>𝐒𝐓𝐀𝐓𝐔𝐒</u>
+<b>• FROM CHANNEL :</b> {}
+<b>• TO CHANNEL :</b> {}
+"""
+      AUTO_FORWARD_STATUS = """
+I'm forward your files from one channel to other channel when a new post is available.
+
+⭕️ <u>𝐒𝐓𝐀𝐓𝐔𝐒</u>
+<b>• FROM CHANNELS :</b> {}
+<b>• TO CHANNELS :</b> {}
+"""
+      MEDIA_COPY_STATUS = """
+I can copy any media to a destination chat provided!
+
+<b>𝖶𝖮𝖱𝖪𝖨𝖭𝖦:</b>
+• Add The Bot to Your destination channel as admin
+• You can add Bot as admin to any chat (Groups or Channels)
+• Bot will copy any media content send in to the above chats to the destination Channel !
+•For the above, You need to give a valid channel Id starting with '-100' in the var "Channel_ID"
+
+⭕️ <u>𝐒𝐓𝐀𝐓𝐔𝐒</u>
+<b>• CURRENT ID</b> : {}
 """
 
-      NOT_ADMIN_TEXT = """
-🔰 <u>𝐒𝐎𝐑𝐑𝐘</u>
-Only my admin can see my status, if you want this bot you can take my source code and deploy it. 
-<b><a href="https://t.me/Ts_bots">©️ ᴛʀᴀᴄᴋsᴛᴜᴅɪᴏ's ʙᴏᴛs</a></b>
-"""
+      AUTO_FILTER = """
+I can search for the files from provided channel according to given query and gives link to those files as buttons!
+
+      HELP_TXT = """<b>Follow These Steps!!</b>
+<b>• First deploy your files forward bot.
+<b>• Then give admin permission in your personal telegram channel</b>
+<b>• Then send any message In your personal telegram channel</b>
+<b>• Then use /run command in your bot</b>
