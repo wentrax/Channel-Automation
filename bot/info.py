1285f295a9b4a08758f4624f97952e45fbb5d391
A - L - E - X <93909675+Lx0988@users.noreply.github.com>
2022-12-22 01:34:23 +0530

    Update info.py

--- bot/info.py
+++ bot/info.py
@@ -6,6 +6,6 @@ ADMIN_ID = 1903280447
 
 DB_URL = "mongodb+srv://Channel-Automation-Robot:seCWS0GPZS4WIveQ@cluster0.r8flwuy.mongodb.net/?retryWrites=true&w=majority"
      
-FROM_CHANNEL = ""
+FROM_CHANNEL = "@bdbdhdhvh" 
       
-TO_CHANNEL = 
+TO_CHANNEL = -1001531149575
