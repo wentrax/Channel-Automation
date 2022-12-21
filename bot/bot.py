588eae7f8baffc78c840b49929938aa2a5e76d8c
A - L - E - X <93909675+Lx0988@users.noreply.github.com>
2022-12-22 02:13:15 +0530

    Update bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -1,6 +1,6 @@
 from pyrogram import Client, enums, __version__
 from . import API_HASH, APP_ID, LOGGER, BOT_TOKEN 
-# from Automation.user import User
+from .clientr import User
 
 class Robot(Client):
 #    USER: User = None
