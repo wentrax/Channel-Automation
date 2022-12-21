ac85fd48e48748ced171d84e1ef56ac6946db35a
A - L - E - X <93909675+Lx0988@users.noreply.github.com>
2022-12-21 19:59:47 +0530

    Delete AutoForward directory

--- AutoForward/__main__.py
+++
@@ -1,6 +0,0 @@
-from AutoForward.client import User
-
-# print("User started") 
-app = User()
-app.run()
-
--- AutoForward/plugins/start.py
+++
@@ -1,6 +0,0 @@
-# @Lx0988
-from pyrogram import Client, filters
-
-@Client.on_message(filters.command("start") & filters.incoming)
-async def start(bot, message):
-        await message.reply(" Hello, I'm Auto Forward Bot")
