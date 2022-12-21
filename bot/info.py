e59d9f4e531fca50d8dce6ce612d71c53a852527
A - L - E - X <93909675+Lx0988@users.noreply.github.com>
2022-12-22 01:32:48 +0530

    Update info.py

--- bot/info.py
+++ bot/info.py
@@ -1,6 +1,11 @@
+CAPTION_POSITION = "bottom"
 
-class Config(object): 
-      CAPTION_POSITION = "bottom"
-      ADMIN_USERNAME = "TTYUIOPTSREW"
-      ADMIN_ID = 1903280447
-      DB_URL = "mongodb+srv://Channel-Automation-Robot:seCWS0GPZS4WIveQ@cluster0.r8flwuy.mongodb.net/?retryWrites=true&w=majority"
+ADMIN_USERNAME = "TTYUIOPTSREW"
+
+ADMIN_ID = 1903280447
+
+DB_URL = "mongodb+srv://Channel-Automation-Robot:seCWS0GPZS4WIveQ@cluster0.r8flwuy.mongodb.net/?retryWrites=true&w=majority"
+     
+FROM_CHANNEL = ""
+      
+TO_CHANNEL = 
