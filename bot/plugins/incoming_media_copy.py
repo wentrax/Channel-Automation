a487d0e8491b1f61f4a28fb261f0c3ac0b1eb01f
A - L - E - X <93909675+Lx0988@users.noreply.github.com>
2022-12-22 21:07:49 +0530

    Update incoming_media_copy.py

--- bot/plugins/incoming_media_copy.py
+++ bot/plugins/incoming_media_copy.py
@@ -12,15 +12,8 @@ async def forward(bot, update):
             chat_id=COPY,
             from_chat_id=update.chat.id,
             message_id=update.id,
-            caption=update.caption,
+            caption=<b>update.caption</b> + "\n\n" "<b>@HQFilms4U</b>,       
             parse_mode=enums.ParseMode.MARKDOWN
-        )      
-        await bot.copy_message(
-            chat_id=-1001531149575,
-            from_chat_id=-1001743048821,
-            message_id=update.id,
-            caption=update.caption,
-            parse_mode=enums.ParseMode.MARKDOWN
-        )
+        )              
     except FloodWait as e:
         await asyncio.sleep(e.value)
