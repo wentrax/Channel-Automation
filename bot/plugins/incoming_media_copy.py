b7226ad400c539b77773f3c45a7dfe9fbfd12957
A - L - E - X <93909675+Lx0988@users.noreply.github.com>
2022-12-22 13:18:10 +0530

    Update incoming_media_copy.py

--- bot/plugins/incoming_media_copy.py
+++ bot/plugins/incoming_media_copy.py
@@ -3,7 +3,7 @@ from pyrogram.errors import FloodWait
 from pyrogram import Client, filters, enums
 from bot.importss import Config 
 
-COPY = Config.MEDIA_COPY_ID
+COPY = Config.MEDIACOPY_ID
 
 @Client.on_message(filters.media)
 async def forward(bot, update):
