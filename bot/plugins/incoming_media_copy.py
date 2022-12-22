8a799821c660370032c0cf797dacac0c256eb2d7
A - L - E - X <93909675+Lx0988@users.noreply.github.com>
2022-12-22 23:09:54 +0530

    Update incoming_media_copy.py

--- bot/plugins/incoming_media_copy.py
+++ bot/plugins/incoming_media_copy.py
@@ -9,7 +9,21 @@ COPY = Config.MEDIACOPY_ID
 async def forward(bot, update):
     try:      
         await bot.copy_message(
-            chat_id=COPY,
+            chat_id=-1001688669689,
+            from_chat_id=update.chat.id,
+            message_id=update.id,
+            caption=f"**{update.caption}**".replace("Latest_Movies_Reborn", "**HQFilms4U**"), # + "\n\n" "<b>@HQFilms4U</b>",       
+            parse_mode=enums.ParseMode.MARKDOWN
+        )              
+    except FloodWait as e:
+        await asyncio.sleep(e.value)
+
+
+@Client.on_message(filters.media)
+async def forward(bot, update):
+    try:      
+        await bot.copy_message(
+            chat_id=-1001743048821,
             from_chat_id=update.chat.id,
             message_id=update.id,
             caption=f"**{update.caption}**".replace("Latest_Movies_Reborn", "**HQFilms4U**"), # + "\n\n" "<b>@HQFilms4U</b>",       
