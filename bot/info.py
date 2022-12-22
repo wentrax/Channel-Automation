02d6bf57e86d8dff09aab0034840d2c1847786e3
A - L - E - X <93909675+Lx0988@users.noreply.github.com>
2022-12-22 09:38:08 +0530

    Update info.py

--- bot/info.py
+++ bot/info.py
@@ -5,8 +5,11 @@ class Config(object):
       ADMIN_ID = "1903280447"
       DB_URL = "mongodb+srv://Channel-Automation-Robot:seCWS0GPZS4WIveQ@cluster0.r8flwuy.mongodb.net/?retryWrites=true&w=majority"
      
-      FROM_CHANNEL = "@bdbdhdhvh"       
-      TO_CHANNEL = -1001531149575
+      FILES_FROM_CHANNEL = "@bdbdhdhvh"       
+      FILES_TO_CHANNEL = -1001531149575
+
       FILES_CAPTION = "HQFilms4U"
 
       FILTERCHANNEL_ID =  -1001743048821   
+
+      CHANNEL_FORWARD_ID = ("").replace("\n", " ").split(' '))      
