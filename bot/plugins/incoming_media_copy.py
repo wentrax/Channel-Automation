0c475bb0295d4ebdf53717362101512442f47068
A - L - E - X <93909675+Lx0988@users.noreply.github.com>
2022-12-22 09:26:25 +0530

    Create incoming_media_copy.py

---
+++ bot/plugins/incoming_media_copy.py
@@ -0,0 +1,19 @@
+import os, asyncio
+from pyrogram.errors import FloodWait
+from pyrogram import Client, filters, enums
+from bot.importss import Config 
+
+COPY = Config.MEDIA_COPY_ID
+
+@Client.on_message(filters.media)
+async def forward(bot, update):
+    try:      
+        await bot.copy_message(
+            chat_id=COPY,
+            from_chat_id=update.chat.id,
+            message_id=update.id,
+            caption=update.caption,
+            parse_mode=enums.ParseMode.MARKDOWN
+        )
+    except FloodWait as e:
+        await asyncio.sleep(e.value)
