866ff9d6f9541174cbef13c3cc22f5c2e0b713f9
A - L - E - X <93909675+Lx0988@users.noreply.github.com>
2022-12-24 04:34:59 +0530

    Update start.py

--- bot/plugins/start.py
+++ bot/plugins/start.py
@@ -11,7 +11,7 @@ from bot.importss import Translation, Robot, Config
 async def start(bot, update):
       await bot.send_message(
           chat_id = update.chat.id,
-          text = Translation.START_TEXT.format(update.from_user.first_name, Config.ADMIN_USERNAME), 
+          text = Translation.START_TEXT.format(update.from_user.first_name,), 
           reply_to_message_id = update.id,
           parse_mode = enums.ParseMode.MARKDOWN,
           disable_web_page_preview = True,           
