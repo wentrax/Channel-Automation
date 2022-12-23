790a1b198e7e486b043ad689301a5eb2370ba211
A - L - E - X <93909675+Lx0988@users.noreply.github.com>
2022-12-24 05:07:46 +0530

    Create auto_caption.py

---
+++ bot/plugins/auto_caption.py
@@ -0,0 +1,39 @@
+import asyncio
+import logging
+from pyrogram import Client, filters, enums
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+TEXT = "@HQFilms4U"
+media_filter = filters.document | filters.video | filters.audio
+
+
+@Client.on_message(filters.chat([-1001743048821, -1001667023505]) & (media_filter))
+async def editing(bot, message):
+      try:
+         media = message.document or message.video or message.audio
+         caption_text = TEXT
+      except:
+         caption_text = ""
+         pass 
+      if (message.document or message.video or message.audio): 
+          if message.caption:                        
+             file_caption = f"**{message.caption}**"                
+          else:
+             fname = media.file_name
+             filename = fname.replace("_", ".")
+             file_caption = f"`{filename}`"  
+              
+      try:
+          if caption_position == "top":
+             await bot.edit_message_caption(
+                 chat_id=message.chat.id, 
+                 message_id=message.id,
+                 caption=file_caption + "\n" + caption_text,
+                 parse_mode=enums.ParseMode.MARKDOWN
+             )
+
+      except:
+          pass
