0b86f4b4dc2ec76816a6acb2b61a715e59dbe563
A - L - E - X <93909675+Lx0988@users.noreply.github.com>
2022-12-24 04:05:05 +0530

    Update autoedit.py

--- bot/plugins/autoedit.py
+++ bot/plugins/autoedit.py
@@ -6,12 +6,18 @@ logger = logging.getLogger(__name__)
 from pyrogram import filters, enums
 from bot.importss import Robot, Config
 
+
+usercaption_position = "bottom"
+caption_position = usercaption_position.lower()
+caption_text = Config.CAPTION_TEXT
+
 CNL = Config.CAPTION_CHANNEL
 media_filter = filters.document | filters.video | filters.audio
 
 @Robot.on_message(filters.chat(CNL) & media_filter)
 async def editing(bot, message):      
       try:
+         media = message.document or message.video or message.audio
          caption_text = Config.CAPTION_TEXT
       except:
          caption_text = ""
@@ -20,14 +26,32 @@ async def editing(bot, message):
           if message.caption:                        
              file_caption = f"**{message.caption}**"                
           else:
-             file_caption = ""           
-                                                 
-      try:                          await bot.edit_message_caption(
+             fname = media.file_name
+             filename = fname.replace("_", ".")
+             file_caption = f"`{filename}`"  
+              
+      try:
+          if caption_position == "top":
+             await bot.edit_message_caption(
+                 chat_id = message.chat.id, 
+                 message_id = message.id,
+                 caption = caption_text + "\n" + file_caption,
+                 parse_mode=enums.ParseMode.MARKDOWN
+             )
+          elif caption_position == "bottom":
+             await bot.edit_message_caption(
                  chat_id = message.chat.id, 
                  message_id = message.id,
                  caption = file_caption + "\n\n" + caption_text,
                  parse_mode=enums.ParseMode.MARKDOWN
              )
+          elif caption_position == "nil":
+             await bot.edit_message_caption(
+                 chat_id = message.chat.id,
+                 message_id = message.id,
+                 caption = caption_text, 
+                 parse_mode=enums.ParseMode.MARKDOWN
+             ) 
       except:
           pass
 
