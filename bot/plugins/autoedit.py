da71261d082bcff057b1994570b549b2b9d42374
A - L - E - X <93909675+Lx0988@users.noreply.github.com>
2022-12-24 04:18:46 +0530

    Update autoedit.py

--- bot/plugins/autoedit.py
+++ bot/plugins/autoedit.py
@@ -1,3 +1,4 @@
+"""
 import logging
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -55,3 +56,4 @@ async def editing(bot, message):
       except:
           pass
 
+"""
