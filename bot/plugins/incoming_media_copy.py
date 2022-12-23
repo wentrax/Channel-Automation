54ae2f55990f8698fb79376a9e05a4ba22601daf
A - L - E - X <93909675+Lx0988@users.noreply.github.com>
2022-12-23 08:09:40 +0530

    Update incoming_media_copy.py

--- bot/plugins/incoming_media_copy.py
+++ bot/plugins/incoming_media_copy.py
@@ -8,7 +8,13 @@ COPY = Config.MEDIACOPY_ID
 @Client.on_message(filters.media)
 async def forward(bot, update):
     try:      
-        await bot.copy_message(chat_id=-1001793777118, from_chat_id=-1001688669689, message_id=update.id, caption=update.caption, parse_mode=enums.ParseMode.MARKDOWN)                     
+        await bot.copy_message(
+            chat_id=-1001427335527, 
+            from_chat_id=-1001531149575, 
+            message_id=update.id, 
+            caption=f"**{update.caption}**.replace ("Latest_Movies_Reborn", "**DXClassic**"), 
+            parse_mode=enums.ParseMode.MARKDOWN                     
+        )
     except FloodWait as e:
         await asyncio.sleep(e.value)
 
