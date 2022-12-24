7004ddb991aeea07d3fa04b7da289705b1d3da08
A - L - E - X <93909675+Lx0988@users.noreply.github.com>
2022-12-24 12:15:30 +0530

    Update files_forward.py

--- bot/plugins/files_forward.py
+++ bot/plugins/files_forward.py
@@ -11,11 +11,10 @@ TO = Config.FILES_TO_CHANNEL
 
 rpl1c = "Hollywood_0980"
 rpl3c = "LkLMNL_09870"
-rpl4c = "DFF_UPDATE"
-rpl5c = "DFF_UPD"
+rpl4c = "DFF_UPDATES"
 
 rplc1d = "DXClassic"
-rplc2d = "Only1DX"
+rplc2d = "Only1DXFEERTS"
 document = enums.MessagesFilter.VIDEO
 
 @Client.on_message(filters.private & filters.command(["start_forward"]))
@@ -47,7 +46,7 @@ async def run(bot, message):
                 chat_id=TO,
                 from_chat_id=FROM,
                 parse_mode=enums.ParseMode.MARKDOWN,       
-                caption=f"**{message.caption}**".replace(rpl1c, rplc1d).replace(rpl3c, rplc1d).replace(rpl4c, rplc2d).replace(rpl5c, rplc2d).replace("ATES", " "),
+                caption=f"**{message.caption}**".replace(rpl1c, rplc1d).replace(rpl3c, rplc1d).replace(rpl4c, rplc2d).replace(rpl5c, rplc2d).replace("FEERTS", " "),
                 message_id=message.id
             )
             files_count += 1
