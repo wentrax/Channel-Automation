7b793615af35d9164ed086609036f77c73e416c3
A - L - E - X <93909675+Lx0988@users.noreply.github.com>
2022-12-26 06:58:25 +0530

    Update auto_forward.py

--- bot/plugins/auto_forward.py
+++ bot/plugins/auto_forward.py
@@ -20,3 +20,15 @@ async def forward(bot, update):
 
 
 
+@Client.on_message(filters.chat(-1001667023505) & media_filter)
+async def forward(bot, update):
+    try:      
+        await bot.copy_message(
+            chat_id=-1001531149575, 
+            from_chat_id=-1001667023505, 
+            message_id=update.id, 
+            caption=f"**{update.caption}**".replace("Latest_Movies_Reborn", "DXClassic"),             
+            parse_mode=enums.ParseMode.MARKDOWN                     
+        )
+    except FloodWait as e:
+        await asyncio.sleep(e.value)
