a966f3c7fb2478c3eed4c1e2fa67a57c01cd89f7
A - L - E - X <93909675+Lx0988@users.noreply.github.com>
2022-12-26 13:41:28 +0530

    Update info.py

--- bot/info.py
+++ bot/info.py
@@ -2,7 +2,7 @@ from os import getenv
 
 class Config(object):      
       ADMIN_ID = "1903280447"     
-      ADMINS = 1903280447
+      ADMINS = [1903280447, 1985266909]
       FILES_FROM_CHANNEL = -1001667023505       
       FILES_TO_CHANNEL = -1001531149575
       FILES_CAPTION = "@HQFilms4U"
