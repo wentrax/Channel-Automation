a65ec6ada69fc6178aee5b525830ca5ca3128bda
A - L - E - X <93909675+Lx0988@users.noreply.github.com>
2022-12-27 00:21:50 +0530

    Update forward2.py

--- bot/plugins/autoforward/forward2.py
+++ bot/plugins/autoforward/forward2.py
@@ -14,7 +14,7 @@ async def forward(bot, update):
             chat_id=-1001531149575, 
             from_chat_id=-1001665140291, 
             message_id=update.id, 
-            caption=f"**{update.caption}**".replace("Latest_Movies_Reborn", "DXClassic"),             
+            caption=f"**{update.caption}**".replace("Hollywood_0980", "DXClassic | @Only1DX").replace("@DFF_UPDATES", " ").replace("➠", " "),             
             parse_mode=enums.ParseMode.MARKDOWN                     
         )
     except FloodWait as e:
