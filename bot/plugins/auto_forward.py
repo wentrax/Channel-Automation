e0837cf80e20076edde049e8ad4dda7f34f64718
A - L - E - X <93909675+Lx0988@users.noreply.github.com>
2022-12-26 13:32:02 +0530

    Update auto_forward.py

--- bot/plugins/auto_forward.py
+++ bot/plugins/auto_forward.py
@@ -18,4 +18,28 @@ async def forward(bot, update):
     except FloodWait as e:
         await asyncio.sleep(e.value)
 
+@Client.on_message(filters.chat(-1001665140291) & media_filter)
+async def forward(bot, update):
+    try:      
+        await bot.copy_message(
+            chat_id=-1001531149575, 
+            from_chat_id=-1001665140291, 
+            message_id=update.id, 
+            caption=f"**{update.caption}**".replace("Hollywood_0980", "DXClassic | @Only1DX").replace("@DFF_UPDATES", " ").replace("➠", " "),             
+            parse_mode=enums.ParseMode.MARKDOWN                     
+        )
+    except FloodWait as e:
+        await asyncio.sleep(e.value)
 
+@Client.on_message(filters.chat(-1001427335527) & media_filter)
+async def forward(bot, update):
+    try:      
+        await bot.copy_message(
+            chat_id=-1001667023505, 
+            from_chat_id=-1001427335527, 
+            message_id=update.id, 
+            caption=f"**{update.caption}**".replace("Hollywood_0980", "DXClassic | @Only1DX").replace("@DFF_UPDATES", " ").replace("➠", " "),             
+            parse_mode=enums.ParseMode.MARKDOWN                     
+        )
+    except FloodWait as e:
+        await asyncio.sleep(e.value)
