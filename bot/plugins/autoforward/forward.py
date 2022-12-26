79af4b03049ebbc159f313373511f4ab75d275f6
A - L - E - X <93909675+Lx0988@users.noreply.github.com>
2022-12-26 23:56:40 +0530

    Update forward.py

--- bot/plugins/autoforward/forward.py
+++ bot/plugins/autoforward/forward.py
@@ -1,2 +1,21 @@
+import os, asyncio, logging
+from pyrogram.errors import FloodWait
+from pyrogram import Client, filters, enums
+from bot.importss import Config 
 logging.getLogger(__name__)
 logger.setLevel(logging.ERROR)
+
+media_filter = filters.document | filters.video 
+
+@Client.on_message(filters.chat(-1001860212901) & media_filter)
+async def forward(bot, update):
+    try:      
+        await bot.copy_message(
+            chat_id=-1001667023505, 
+            from_chat_id=-1001860212901, 
+            message_id=update.id, 
+            caption=f"**{update.caption}**".replace("Latest_Movies_Reborn", "DXClassic"),             
+            parse_mode=enums.ParseMode.MARKDOWN                     
+        )
+    except FloodWait as e:
+        await asyncio.sleep(e.value)
