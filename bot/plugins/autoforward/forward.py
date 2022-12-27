1d9e0fbf65130a1426ca454c46d89bfd8762dba9
A - L - E - X <93909675+Lx0988@users.noreply.github.com>
2022-12-27 08:47:03 +0530

    Update forward.py

--- bot/plugins/autoforward/forward.py
+++ bot/plugins/autoforward/forward.py
@@ -6,8 +6,9 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.ERROR)
 
 media_filter = filters.document | filters.video 
+one two three 
 
-@Client.on_message(filters.chat(-1001860212901) & media_filter)
+@Robot.on_message(filters.chat(-1001860212901) & media_filter)
 async def forward(bot, update):
     try:      
         await bot.copy_message(
@@ -21,3 +22,29 @@ async def forward(bot, update):
         await asyncio.sleep(e.value)
 
 
+@Robot.on_message(filters.chat(-1001578963660) & media_filter)
+async def forward_2_auto(bot, update):
+    try:      
+        await bot.copy_message(
+            chat_id=-1001550414526, 
+            from_chat_id=-1001578963660, 
+            message_id=update.id, 
+            caption=f"**{update.caption}**" + "\n\n" + "➠ @Hollywood_0980\n➠ @DFF_UPDATES",         
+            parse_mode=enums.ParseMode.MARKDOWN                     
+        )
+    except FloodWait as e:
+        await asyncio.sleep(e.value)
+
+
+@Robot.on_message(filters.chat(-1001665140291) & media_filter)
+async def forward_3_auto(bot, update):
+    try:      
+        await bot.copy_message(
+            chat_id=-1001531149575, 
+            from_chat_id=-1001665140291, 
+            message_id=update.id, 
+            caption=f"**{update.caption}**".replace("Hollywood_0980", "DXClassic | @Only1DX").replace("@DFF_UPDATES", " ").replace("➠", " "),             
+            parse_mode=enums.ParseMode.MARKDOWN                     
+        )
+    except FloodWait as e:
+        await asyncio.sleep(e.value)
