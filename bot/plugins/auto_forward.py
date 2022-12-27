f67e887cfed6f7c0b124363b23335194cd70e3c2
A - L - E - X <93909675+Lx0988@users.noreply.github.com>
2022-12-27 11:51:26 +0530

    Update auto_forward.py

--- bot/plugins/auto_forward.py
+++ bot/plugins/auto_forward.py
@@ -11,7 +11,7 @@ media_filter = filters.document | filters.video
 async def forward(bot, update):
     try:      
         await bot.copy_message(
-            chat_id=-1001427335527, 
+            chat_id=-1001840022987, 
             from_chat_id=-1001641840781, 
             message_id=update.id, 
             caption=f"**{update.caption}**".replace("Latest_Movies_Reborn", " ").replace ("🔥 Fɪʀꜱᴛ Oɴ Tᴇʟᴇɢʀᴀᴍ 🔥", " ").replace("@BollyWeeBHolly", " ").replace("@JESSEVERSE", " ").replace("@Theprofffesorr", " ") + "**@DXClassic**",             
