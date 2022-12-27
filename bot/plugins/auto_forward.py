5fd62d974262ef51580f03eab957e979f73ee10a
A - L - E - X <93909675+Lx0988@users.noreply.github.com>
2022-12-27 13:11:35 +0530

    Update auto_forward.py

--- bot/plugins/auto_forward.py
+++ bot/plugins/auto_forward.py
@@ -7,16 +7,28 @@ logger.setLevel(logging.ERROR)
 
 media_filter = filters.document | filters.video 
 
-@Client.on_message(filters.chat(-1001641840781) & media_filter)
+@Client.on_message(filters.chat(-1001857977699) & media_filter)
 async def forward(bot, update):
     try:      
         await bot.copy_message(
-            chat_id=-1001840022987, 
-            from_chat_id=-1001641840781, 
+            chat_id=-1001846691219, 
+            from_chat_id=-1001857977699, 
             message_id=update.id, 
-            caption=f"**{update.caption}**".replace("@Latest_Movies_Reborn", " ").replace ("🔥 Fɪʀꜱᴛ Oɴ Tᴇʟᴇɢʀᴀᴍ 🔥", " ").replace("@BollyWeeBHolly", " ").replace("@JESSEVERSE", " ").replace("@Theprofffesorr", " ").replace("\n\n\n", " ") + "\n\n" + "**@DXClassic**",          
+            caption=f"**{update.caption}**".replace("@Latest_Movies_Reborn", " ").replace ("🔥 Fɪʀꜱᴛ Oɴ Tᴇʟᴇɢʀᴀᴍ 🔥", " ").replace("@BollyWeeBHolly", " ").replace("@JESSEVERSE", " ").replace("@Theprofffesorr", " "),          
             parse_mode=enums.ParseMode.MARKDOWN                     
         )
     except FloodWait as e:
         await asyncio.sleep(e.value)
 
+@Client.on_message(filters.chat(-1001641840781) & media_filter)
+async def forward(bot, update):
+    try:      
+        await bot.copy_message(
+            chat_id=-1001427335527, 
+            from_chat_id=-1001641840781, 
+            message_id=update.id, 
+            caption=f"**{update.caption}**" + "\n\n" + "**@DXClassic**",         
+            parse_mode=enums.ParseMode.MARKDOWN                     
+        )
+    except FloodWait as e:
+        await asyncio.sleep(e.value)
