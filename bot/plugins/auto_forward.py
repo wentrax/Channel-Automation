c07047b6f32cbed9674dcf3599007fc47fd29961
A - L - E - X <93909675+Lx0988@users.noreply.github.com>
2022-12-29 17:43:12 +0530

    Update auto_forward.py

--- bot/plugins/auto_forward.py
+++ bot/plugins/auto_forward.py
@@ -20,7 +20,6 @@ async def forward(bot, update):
     except FloodWait as e:
         await asyncio.sleep(e.value)
 
-"""
 @Client.on_message(filters.chat(-1001427335527) & media_filter)
 async def forward_2_auto(bot, update):
     try:      
@@ -33,4 +32,3 @@ async def forward_2_auto(bot, update):
         )
     except FloodWait as e:
         await asyncio.sleep(e.value)
-"""
