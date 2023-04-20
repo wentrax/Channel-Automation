d406ef1478d286b5f4b230109a3c7ad952a9fb25
WENTRAX <127533917+wentrax@users.noreply.github.com>
2023-04-20 23:36:45 +0530

    Update info.py

--- bot/info.py
+++ bot/info.py
@@ -1,8 +1,8 @@
 from os import getenv
 
 class Config(object):      
-      ADMIN_ID = "1903280447"     
-      ADMINS = [1903280447, 1985266909]
+      ADMIN_ID = "1533128706"     
+      ADMINS = [1533128706, 1985266909]
       FILES_FROM_CHANNEL = -1001667023505       
       FILES_TO_CHANNEL = -1001929344696
       FILES_CAPTION = "@HQFilms4U"
