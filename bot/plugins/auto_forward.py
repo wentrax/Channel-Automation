f52b40e7c58acf1058d278880435b0600accfd80
WENTRAX <127533917+wentrax@users.noreply.github.com>
2023-04-20 23:46:24 +0530

    Update auto_forward.py

--- bot/plugins/auto_forward.py
+++ bot/plugins/auto_forward.py
@@ -7,30 +7,16 @@ logger.setLevel(logging.ERROR)
 
 media_filter = filters.document | filters.video 
 
-@Client.on_message(filters.chat(-1001857977699) & media_filter)
+@Client.on_message(filters.chat(-1001720280064) & media_filter)
 async def forward(bot, update):
     try:      
         await bot.copy_message(
-            chat_id=-1001846691219, 
-            from_chat_id=-1001857977699, 
+            chat_id=-1001929344696, 
+            from_chat_id=-1001720280064, 
             message_id=update.id, 
-            caption=f"**{update.caption}**".replace("@Latest_Movies_Reborn", " ").replace ("🔥 Fɪʀꜱᴛ Oɴ Tᴇʟᴇɢʀᴀᴍ 🔥", " ").replace("@BollyWeeBHolly", " ").replace("@JESSEVERSE", " ").replace("@Theprofffesorr", " ").replace("🔥 Join Us On Telegram 🔥", " ").replace("@File_Movies_Uploaded", " "),          
+            caption=update.caption, 
             parse_mode=enums.ParseMode.MARKDOWN                     
         )
     except FloodWait as e:
         await asyncio.sleep(e.value)
 
-"""
-@Client.on_message(filters.chat(-1001427335527) & media_filter)
-async def forward_2_auto(bot, update):
-    try:      
-        await bot.copy_message(
-            chat_id=-1001667023505, 
-            from_chat_id=-1001427335527, 
-            message_id=update.id, 
-            caption=f"**{update.caption}**".replace("@DXClassic", "➠ @Hollywood_0980\n➠ @DFF_UPDATES"),
-            parse_mode=enums.ParseMode.MARKDOWN                     
-        )
-    except FloodWait as e:
-        await asyncio.sleep(e.value)
-"""
